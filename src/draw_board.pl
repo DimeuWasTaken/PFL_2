24017dabb02d57f1aedef2da7fa7e434f4f28200
Dimeu <diogop.2001@gmail.com>
2022-01-14 15:51:54 +0000

    verifying input

--- src/draw_board.pl
+++ src/draw_board.pl
@@ -1,34 +1,31 @@
 :- consult('draw_main_menu.pl').
 
+draw_cell(0) :- write('_').
+draw_cell(1) :- write('o').
+draw_cell(-1) :- write('x').
+
 draw_x([]).
-draw_x([X | N]) :- (X >= 0 ->
-                   write(' ');
-                   true), write(X), write(' '), draw_x(N).
+draw_x([X | N]) :- write(' '), draw_cell(X), write(' '), draw_x(N).
 
-draw_y([], _)  :- nl, nl, write('                X'), nl.
-draw_y([Y | N], Line) :- nl, (Line == 4 -> write(' Y ') ; write('   ')), draw_x(Y), Line2 is Line + 1, draw_y(N, Line2).
+draw_y([], _)  :- 
+    nl, write('        ----------------------------'), 
+    nl, write('          0  1  2  3  4  5  6  7  8 '), 
+    nl, write('        ----------------------------'), 
+    nl, write('                      X'), nl.
+draw_y([Y | N], Line) :- 
+    nl, 
+    (Line == 4 -> 
+    write(' Y | '); 
+    write('   | ')), 
+    write(Line), write(' | '), draw_x(Y), 
+    Line2 is Line + 1, draw_y(N, Line2).
 
-select_move(Player) :- 
-    write('choose X to play: (0 - 8)'), nl,
-    read(Input_x),
-    write('choose Y to play: (0 - 8)'), nl,
-    read(Input_y),
-    write('move chosen: X = '), write(Input_x), write(' , Y = '), write(Input_y), nl, process_move(Player, Input_x, Input_y).
+display_game(GameState) :- FirstLine = 0,
+              draw_y(GameState, FirstLine), nl.
 
-process_move(Player, X, Y) :-
-    ((X > 8 ; X < 0 ; Y > 8 ; Y < 0) ->
-    write('input error'), nl, select_move(Player);
-    true).
-
-draw_initial_board :-   Initial_board = [
-                            [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-                            [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                            [1, 1, 1, 1, 1, 1, 1, 1, 1], 
-                            [1, 1, 1, 1, 1, 1, 1, 1, 1]
-                        ], First_line = 0,
-                        draw_y(Initial_board, First_line), nl.
+match([H|_],0,H) :-
+    !.
+match([_|T],N,H) :-
+    N > 0, %add for loop prevention
+    N1 is N-1,
+    match(T,N1,H).
--- src/renpaarden.pl
+++ src/renpaarden.pl
@@ -1,4 +1,45 @@
 :- consult('draw_main_menu.pl').
 :- consult('draw_board.pl').
 
-play :- Current_player = 1, draw_main_menu, draw_initial_board, select_move(Current_player).
+play :- Board = [
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [1, 1, 1, 1, 1, 1, 1, 1, 1], 
+    [1, 1, 1, 1, 1, 1, 1, 1, 1]],
+    draw_main_menu, game_cycle(Board).
+    
+game_cycle(Board) :-
+    Current_player = 1, 
+    
+    display_game(Board),
+    move(Board, Current_player).
+
+move(GameState, Current_player) :- 
+    repeat,
+    write('choose X to play: (0 - 8)'), nl,
+    read(Input_x),
+    write('choose Y to play: (0 - 8)'), nl,
+    read(Input_y),
+
+    ((Input_x > 8 ; Input_x < 0 ; Input_y > 8 ; Input_y < 0) ->
+    write('input error'), nl, fail;
+    true),
+
+    match(GameState, Input_y, I), match(I, Input_x, J),
+
+    (((Current_player == 1, J == 1); (Current_player == 2, J == -1))->
+    !;
+    write('invalid cell, not your piece'), nl, fail),
+    
+    write('position '), write(Input_x), write(':'), write(Input_y), write(' - '), draw_cell(J).
+
+test(I) :-
+    Y = 5,
+    Z = 1,
+    I is Y + Z,
+    write(I), nl.
